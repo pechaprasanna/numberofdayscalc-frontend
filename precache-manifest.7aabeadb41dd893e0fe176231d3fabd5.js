fda648ea870433d0fd41e8291c672e09ef7a9cfb
pechaprasanna <rpv31dec1991@gmail.com>
2020-07-02 09:01:45 -0700

    Updates

---
+++ precache-manifest.7aabeadb41dd893e0fe176231d3fabd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef33d163ada7c065be1db6a49dc2458",
+    "url": "/numberofdayscalc-frontend/index.html"
+  },
+  {
+    "revision": "b5ff09a1ecf21183a6b6",
+    "url": "/numberofdayscalc-frontend/static/css/2.96388320.chunk.css"
+  },
+  {
+    "revision": "d15d1619a4dba8c61201",
+    "url": "/numberofdayscalc-frontend/static/css/main.0a48c935.chunk.css"
+  },
+  {
+    "revision": "b5ff09a1ecf21183a6b6",
+    "url": "/numberofdayscalc-frontend/static/js/2.fcc2e938.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/numberofdayscalc-frontend/static/js/2.fcc2e938.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15d1619a4dba8c61201",
+    "url": "/numberofdayscalc-frontend/static/js/main.a6aa4c12.chunk.js"
+  },
+  {
+    "revision": "7926293d9d1cae05ad44",
+    "url": "/numberofdayscalc-frontend/static/js/runtime-main.cc404f5b.js"
+  },
+  {
+    "revision": "b032e8f8730e16f5f789173edd2eef4a",
+    "url": "/numberofdayscalc-frontend/static/media/Diavlo_BOLD_II_37.b032e8f8.woff"
+  }
+]);
